10587ff5c91d1dbee375d0893e2f1325547149b5
shivam chaurasiya <cshivam3312@gmail.com>
2023-12-14 17:43:06 +0530

    Matine Ui Configured

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import { MantineProvider, createTheme } from '@mantine/core';
 
 import { api } from "~/utils/api";
 
+// Mantine css
+import '@mantine/core/styles.css';
+
+// Global css
 import "~/styles/globals.css";
+
+// reduxStore provider
 import { Providers } from "~/lib/providers";
+
+// Your theme configuration is merged with default theme
+const theme = createTheme({
+  fontFamily: 'Montserrat, sans-serif',
+  defaultRadius: 'md',
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -14,7 +27,10 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <Providers>
       <SessionProvider session={session}>
-        <Component {...pageProps} />
+        <MantineProvider
+          theme={theme}>
+          <Component {...pageProps} />
+        </MantineProvider>
       </SessionProvider>
     </Providers>
   );
